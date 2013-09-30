183e02ca16d1fcd3d1762658416ba752140c9edd
H. Cole Wiley <hcwiley@gmail.com>
2013-09-30 14:30:32 -0500

    no really fixed it

--- js/client.js
+++ js/client.js
@@ -3,13 +3,9 @@ function bindScroll(){
   $(window).unbind("scroll");
   $(window).on("scroll",function(e){
     var curPos = $(window).scrollTop();
-    var top = $(".navbar").data('top0');
-    if( !top ){
-      top = $(".navbar").position().top;
-      $(".navbar").data('top0', top );
-    }
+    var top = $(".navbar").offset().top;
     //console.log(curPos+", "+top);
-    if(curPos > top ){
+    if(curPos >= top ){
       if(!$(".navbar").hasClass("navbar-fixed-top") ){
         $(".navbar").addClass("navbar-fixed-top");
         $("#about").css("top", $(".navbar").height()+"px");
