05597face553d99dda5ca7e96595130a6be9f00d
H. Cole Wiley <hcwiley@gmail.com>
2014-01-27 12:29:01 -0600

    nearly done with the classes page

--- js/client.js
+++ js/client.js
@@ -26,6 +26,46 @@ function bindScroll(){
   });
 }
 
+
+function stripeResHandler(status, res) {
+  var form;
+  form = $("#enrollment-form");
+  if (res.error) {
+    form.find('.enrollment-errors').text(res.error.message);
+    return form.find('button').prop('disabled', false);
+  } else {
+    form.append($("<input type='hidden' name='stripeToken' value='" + res.id + "'/>"));
+    return form.get(0).submit();
+  }
+};
+
+function bindEnrollmentForm() {
+  $("#num-classes").change(function(){
+    var val = $(this).val();
+    if(val == "5")
+      $("#total").text("7.52");
+    else if(val == "15")
+      $("#total").text("20.91");
+    else if(val == "40")
+      $("#total").text("51.80");
+  });
+  $("#enrollment-form #pay-online").click(function(e) {
+    e.preventDefault();
+    var form;
+    form = $(this).closest('form');
+    form.find('.first-buttons').remove();
+    form.append($("#cc-info").removeClass('hidden'));
+    $("#enrollment-form").submit(function(e) {
+      e.preventDefault();
+      form.find('button').prop('disabled', true);
+      Stripe.card.createToken(form, stripeResHandler);
+      return false;
+    });
+    return false;
+  });
+};
+
 $(window).ready(function(){
   bindScroll();
+  bindEnrollmentForm();
 });
